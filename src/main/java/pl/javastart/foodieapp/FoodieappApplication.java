e3e93b28209919e03d4e8031780982720e216ae1
Aleksander <alek@siudek.net>
2022-05-17 08:49:17 +0200

    learning

---
+++ src/main/java/pl/javastart/foodieapp/FoodieappApplication.java
@@ -0,0 +1,11 @@
+package pl.javastart.foodieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieappApplication {
+    public static void main(String[] args){
+        SpringApplication.run(FoodieappApplication.class);
+    }
+}
